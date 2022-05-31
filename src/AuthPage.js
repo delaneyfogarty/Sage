a74166e00c22f0470556c70c02fcfc1ef552551d
SusanBird <susanbird391@gmail.com>
2022-05-31 10:55:13 -0700

    built auth page

---
+++ src/AuthPage.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { signUpUser, signInUser, getUser } from './services/supabase';
+
+export default function AuthPage({ setEmail, setToken }) {
+  const [signUpEmail, setSignUpEmail] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+
+
+  const [{
+    email: signInEmail, 
+    password: signInPassword,
+  }, setSignInFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  async function handleSignUpSubmit(e) {
+    e.preventDefault();
+
+    await signUpUser(signUpEmail, signUpPassword);
+
+    const {
+      access_token, 
+      user: {
+        email,
+      }
+    } = getUser();
+
+    setEmail(email);
+    setToken(access_token);
+  }
+
+  async function handleSignInSubmit(e) {
+    e.preventDefault();
+
+    await signInUser(signInEmail, signInPassword);
+
+    const {
+      access_token, 
+      user: {
+        email,
+      }
+    } = getUser();
+
+    setEmail(email);
+    setToken(access_token);
+  }
+
+  return (
+    <div className="auth-page">
+      <form onSubmit={handleSignUpSubmit}>
+        <h4>Sign Up</h4>
+        <label>
+                email
+          <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)}/>
+        </label>
+        <label>
+                password
+          <input value={signUpPassword} onChange={e => setSignUpPassword(e.target.value)}/>
+        </label>
+        <button>
+                Sign Up
+        </button>
+      </form>
+      <form onSubmit={handleSignInSubmit}>
+        <h4>Sign In</h4>
+        <label>
+                email
+          <input value={signInEmail} onChange={e => setSignInFormData({
+            email: e.target.value,
+            password: signInPassword,
+          })}/>
+        </label>
+        <label>
+                password
+          <input type="password" value={signInPassword} onChange={e => setSignInFormData({
+            email: signInEmail,
+            password: e.target.value
+          })}/>           
+        </label>
+        <button>
+                Sign In
+        </button>
+      </form>
+    </div>
+  );
+}
